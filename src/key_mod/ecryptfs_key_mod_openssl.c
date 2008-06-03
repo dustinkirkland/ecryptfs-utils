34b82f04472e0540604f84966e02b9ef689a5859
Mike halcrow <mhalcrow@us.ibm.com>
2008-06-03 15:51:54 +0000

    Comment fix

--- src/key_mod/ecryptfs_key_mod_openssl.c
+++ src/key_mod/ecryptfs_key_mod_openssl.c
@@ -785,7 +785,7 @@ static struct param_node ssl_param_nodes[] = {
 	 /* An implicit transition takes place if one of the key
 	  * module parameters that are the target of one of the
 	  * transition nodes already exists in the provided parameter
-	  * list. The provided parameter list */
+	  * list. */
 	 .flags = (DISPLAY_TRANSITION_NODE_VALS | ECRYPTFS_DISPLAY_PRETTY_VALS
 		   | ECRYPTFS_PARAM_FLAG_ECHO_INPUT
 		   | ECRYPTFS_ALLOW_IMPLICIT_TRANSITION),
