cdad1658bb10033113bed0a3769be45b85018b5d
Dustin Kirkland <kirkland@canonical.com>
2008-12-02 03:38:33 +0000

    some cleanup (mv'ing and rm'ing)
    The bootstrap.sh and release.sh are maintainer scripts and really belong
    in the ./scripts directory.
    
    The ecryptfs-setup-pam.sh and ecryptfs-undo-pam.sh scripts are
    completely deprecated now, replaced by the ecryptfs-setup-private
    functionality.
    
    Signed-off-by: Dustin Kirkland <kirkland@canonical.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 # Create and sign a release tarball for upload to
 # https://launchpad.net/ecryptfs/trunk
 
-./bootstrap.sh
+./scripts/bootstrap.sh
 ./configure --prefix=/usr
 make dist
 gpg --armor --sign --detach-sig ecryptfs-utils-*.tar.gz
