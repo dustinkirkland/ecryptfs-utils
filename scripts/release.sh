5037a9adf780fa989793710da8add6c47d2da89d
Dustin Kirkland (none) <kirkland@jaunty.>
2009-01-26 19:30:01 +0000

    improve the release script
    Clean up ahead of time, and print link to upload site.
    
    Signed-off-by: Dustin Kirkland <kirkland@canonical.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@
 # Create and sign a release tarball for upload to
 # https://launchpad.net/ecryptfs/trunk
 
+rm -f ./ecryptfs-utils*.tar.*
 ./scripts/bootstrap.sh
 ./configure --prefix=/usr
 make dist
@@ -11,3 +12,9 @@ for i in `ls ecryptfs-utils-*.tar.gz`; do
 	mv $i ecryptfs-utils_$ver.orig.tar.gz
 done
 gpg --armor --sign --detach-sig ecryptfs-utils_*.orig.tar.gz
+echo
+echo "TO MAKE THE RELEASE OFFICIAL, UPLOAD:"
+echo -n "  "
+ls *.orig.tar.gz
+echo "---->  https://launchpad.net/ecryptfs/trunk"
+echo
