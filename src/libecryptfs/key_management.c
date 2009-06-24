55b47af7e0bbee14f817a37a77d0b85bf790ffec
Michal Hlavinka <mhlavink@redhat.com>
2009-06-24 08:00:39 +0200

    key_management.c: don't fail if key already exists in keyring
    If adding key to keyring and key is already there, it doesn't 
    mean there is any problem, we need that key and we have it.
    
    Signed-off-by: Michal Hlavinka <mhlavink@redhat.com>

--- src/libecryptfs/key_management.c
+++ src/libecryptfs/key_management.c
@@ -676,18 +676,20 @@ int ecryptfs_insert_wrapped_passphrase_into_keyring(
 		if ((rc = ecryptfs_add_passphrase_key_to_keyring(
 					auth_tok_sig,
 					decrypted_passphrase,
-					ECRYPTFS_DEFAULT_SALT_FNEK_HEX))) {
+					ECRYPTFS_DEFAULT_SALT_FNEK_HEX)) < 0) {
 			syslog(LOG_ERR,
 			   "Error attempting to add filename encryption key to "
 			   "user session keyring; rc = [%d]\n", rc);
+			goto out;
 		}
 	}
 	if ((rc = ecryptfs_add_passphrase_key_to_keyring(auth_tok_sig,
 							 decrypted_passphrase,
-							 salt))) {
+							 salt)) < 0) {
 		syslog(LOG_ERR, "Error attempting to add passphrase key to "
 		       "user session keyring; rc = [%d]\n", rc);
-	}
+	} else
+		rc = 0;
 out:
 	return rc;
 }
